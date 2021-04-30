9598a748f096ca36103d531d124dd0a888da4c32
unknown989 <unknown989@protonmail.com>
2021-04-30 22:50:53 +0100

    Removed Qt Creator template

--- dynamicarr/dynamicarr.pro
+++ dynamicarr/dynamicarr.pro
@@ -4,7 +4,7 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
-    main.cpp
+    example.cpp
 
 HEADERS += \
-    main.h
+    dynamicarr.h
--- dynamicarr/example.cpp
+++ dynamicarr/example.cpp
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
-#include "main.h"
+#include "dynamicarr.h"
 
 
 int main(){
-    Array<char*> arr;
-    arr.append("Hello");
+    Array<char*> arr; // Initialize an array
+    arr.append("Hello"); // Append to it
     arr.append("World");
     arr.append(",");
     arr.append("We're");
     arr.append("Identic");
     arr.append("Humans");
 
+    arr.pop(1); // Remove an element from it
+
+	// Loop throw it
     for(int i = 0; i <= arr.get_m_count()-1;i++){
-        std::cout << arr.get(i) << " ";
+        std::cout << arr.get(i) << " "; // Get an element from it
     }
     printf("\n");
 
